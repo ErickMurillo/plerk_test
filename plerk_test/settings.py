28f5a5718ea3bb9db7d77c2e196b7d3b1758d845
Erick Murillo <erickmurillo22@gmail.com>
2022-05-11 14:30:42 -0600

    fix statics

--- plerk_test/settings.py
+++ plerk_test/settings.py
@@ -92,7 +92,16 @@ USE_I18N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+MEDIA_ROOT = os.environ.get('MEDIA_ROOT', os.path.join(BASE_DIR, 'media'))
+# MEDIA_URL = '/media/'
+# CKEDITOR_UPLOAD_PATH = "/uploads/"
+    
+STATIC_ROOT = os.environ.get('STATIC_ROOT', os.path.join(BASE_DIR, 'static'))
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static_media"),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
